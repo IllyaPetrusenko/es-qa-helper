187c11a7a3d089bb372028c72bd0719da42dda2b
IllyaPetrusenko <illya.petrusenko@gmail.com>
2021-09-13 13:34:14 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -380,7 +380,15 @@ def issuing_fc(host, token, x_operation_id, ap_cpid, fe_ocid, contract_id, ap_x_
 # Confirmation response
 def create_confirmation_response(host, token, x_operation_id, x_token, entity, cpid, ocid, entity_id, role,
                                  payload, response_id):
+    document = ''
+    if host == 'http://10.0.20.126:8900/api/v1/':
+        public_point = 'http://dev.public.eprocurement.systems/tenders/'
+        document = 'b5802bf4-b838-431e-831b-7d0ef5ed9437-1593170692555'
+    if host == 'http://10.0.10.116:8900/api/v1/':
+        public_point = 'http://public.eprocurement.systems/tenders/'
+        document = '21a5d5ef-84c0-4730-892c-338db4e3e98d-1631521816681'
     payload['confirmationResponse']['requestId'] = f'{response_id}'
+    payload['confirmationResponse']['businessFunctions'][0]['documents'][0]['id'] = document
     requests.post(url=f'{host}/do/confirmation/{entity}/{cpid}/{ocid}/{entity_id}?role={role}',
                   headers={
                       'Authorization': f'Bearer {token}',
