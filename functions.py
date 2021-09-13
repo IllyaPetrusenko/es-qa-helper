4ced2d44b77a53beb8f6237de074c4972350634b
IllyaPetrusenko <illya.petrusenko@gmail.com>
2021-09-13 11:30:59 +0300

    add document for sandbox

--- functions.py
+++ functions.py
@@ -96,7 +96,7 @@ def create_ap(host, token, x_operation_id, payload, pmd):
     if host == 'http://10.0.20.126:8900/api/v1/':
         document = 'b5802bf4-b838-431e-831b-7d0ef5ed9437-1593170692555'
     if host == 'http://10.0.10.116:8900/api/v1/':
-        document = 'http://public.eprocurement.systems/tenders/'
+        document = '21a5d5ef-84c0-4730-892c-338db4e3e98d-1631521816681'
     payload['tender']['documents'][0]['id'] = document
     requests.post(url=f'{host}/do/ap?country=MD&pmd={pmd}&lang=ro&testMode=true', headers={
         'Authorization': f'Bearer {token}',
