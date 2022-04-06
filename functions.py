42bf872a7fe340bf77c74b7e4dbb9a48eccb8af1
illya.petrusenko@gmail.com <illya.petrusenko@gmail.com>
2022-04-06 11:05:32 +0300

    new version

--- functions.py
+++ functions.py
@@ -159,7 +159,7 @@ def update_ap(host, token, x_operation_id, ap_x_token, ap_cpid, ap_ocid, payload
 
 # Generate periods
 def generate_period():
-    prequalification_period_end = datetime.datetime.now() - datetime.timedelta(hours=2)
+    prequalification_period_end = datetime.datetime.now() - datetime.timedelta(hours=3)
     prequalification_period_end = prequalification_period_end + datetime.timedelta(seconds=25)
     prequalification_period_end = prequalification_period_end.strftime("%Y-%m-%dT%H:%M:%SZ")
     print(str(prequalification_period_end))
