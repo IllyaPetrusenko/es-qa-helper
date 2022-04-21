15ef73e93a6ef69d243a5ec0b6b4364ee31b789a
illya.petrusenko@gmail.com <illya.petrusenko@gmail.com>
2022-04-21 09:34:33 +0300

    fix

--- payloads/create_ac.py
+++ payloads/create_ac.py
@@ -1,7 +1,7 @@
 contract = {
-    "contracts":[
-      {
-        "id": "adf75a7e-8690-4485-ae58-69c2de22bc67"
-      }
+    "contracts": [
+        {
+            "id": "adf75a7e-8690-4485-ae58-69c2de22bc67"
+        }
     ]
 }
