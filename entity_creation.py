e7572906cf641e5d6290c192970a7ec3f1647a64
illya.petrusenko@gmail.com <illya.petrusenko@gmail.com>
2022-04-22 17:13:39 +0300

    add restricted start second stage

--- entity_creation.py
+++ entity_creation.py
@@ -390,7 +390,7 @@ class CreateEntity:
             'X-OPERATION-ID': operation_id,
             'Content-Type': 'application/json',
             'X-TOKEN': token
-        }, data=json.dumps(payload)).json()
+        }, data=json.dumps(payload))
         print(r)
         kafka_message = self.get_message_from_kafka(operation_id)
         print(kafka_message)
