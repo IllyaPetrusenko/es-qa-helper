a0320ba2d57b25a050618cb5902207312bafc1ab
illya.petrusenko@gmail.com <illya.petrusenko@gmail.com>
2022-04-22 18:52:46 +0300

    add restricted start second stage

--- entity_creation.py
+++ entity_creation.py
@@ -232,15 +232,18 @@ class CreateEntity:
     def get_awards(self, cpid, ocid):
         kafka_message = self.get_bpe_message_from_kafka(ocid, 'bpe')
         print(kafka_message)
-        awards = kafka_message[0]['data']['outcomes']
-        if 'awards' in awards:
-            public_awards = requests.get(url=f'{self.public_point}/{cpid}/{ocid}').json()['releases'][0]['awards']
-            for award in public_awards:
-                if award['statusDetails'] == 'awaiting':
-                    award_1 = award['id']
-                    for i in awards:
-                        if i['id'] == award_1:
-                            return award_1, i['X-TOKEN']
+        for i in kafka_message:
+            print(i)
+            # if 'awards' in i['data']['outcomes']:
+            #
+            # if 'awards' in awards:
+            #     public_awards = requests.get(url=f'{self.public_point}/{cpid}/{ocid}').json()['releases'][0]['awards']
+            #     for award in public_awards:
+            #         if award['statusDetails'] == 'awaiting':
+            #             award_1 = award['id']
+            #             for i in awards:
+            #                 if i['id'] == award_1:
+            #                     return award_1, i['X-TOKEN']
 
     def do_consideration(self, award, award_token, cpid, ocid):
         operation_id = self.get_x_operation_id()
