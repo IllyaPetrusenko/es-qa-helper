b2384e4440bc48098d78cf662dc65ca6130165d1
illya.petrusenko@gmail.com <illya.petrusenko@gmail.com>
2022-04-22 10:40:35 +0300

    fix

--- entity_creation.py
+++ entity_creation.py
@@ -283,7 +283,7 @@ class CreateEntity:
             ]
         }
         data['contracts'][0]['id'] = can_id
-        print(dict(data))
+        data = dict(data)
         requests.post(
             url=f'{self.host}do/contract/{cpid}/{ocid}',
             headers={
@@ -292,7 +292,7 @@ class CreateEntity:
                 'Content-Type': 'application/json',
                 'X-TOKEN': token
             },
-            data=dict(data)
+            data=json.dumps(data)
         ).json()
         kafka_message = self.get_message_from_kafka(operation_id)
         return kafka_message
