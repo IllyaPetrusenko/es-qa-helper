196b63939b3d4f703ff7c7e350197bcfaa2507df
anastasiia <nanamarkova@gmail.com>
2022-05-09 09:12:40 +0200

    nastya

--- payloads/create_ei.py
+++ payloads/create_ei.py
@@ -1,64 +1,107 @@
 ei = {
-    "tender": {
-        "title": "tender.title",
-        "description": "tender.description",
-        "classification": {
-            "id": "45200000-9"
-        }
-    },
-    "planning": {
-        "budget": {
-            "period": {
-                "startDate": "2020-06-25T16:12:45Z",
-                "endDate": "2022-12-26T16:12:45Z"
-            }
-        },
-        "rationale": "planning.rationale"
-    },
-    "buyer": {
-        "name": "Школа №1 города Кишинёв",
-        "identifier": {
-            "id": "001",
-            "scheme": "MD-IDNO",
-            "legalName": "Buyer One",
-            "uri": "http://uri.com"
-        },
-        "address": {
-            "streetAddress": "address.streetAddress",
-            "postalCode": "5600",
+	"tender":
+	{
+        "title": "Title of Expenditure Item",
+    	 "description": "Expenditure Item description",
+    	   "classification":
+	    {
+      		"id": "15100000-9"
+    	  },
+           "items": [{
+              "id": "1",
+              "description": "first item in EI",
+              "classification": {
+                  "id": "15110000-2"
+               },
+              "additionalClassifications": [{
+                  "id": "BA43-8"
+              }
+            ],
+              "deliveryAddress":{
+                "streetAddress":"Street EI",
+                "postalCode":"EI postal code",
+                "addressDetails":{
+                    "country":{
+                        "id":"MD"
+                        },
+                    "region":{
+                        "id":"3100000"
+                    },
+                    "locality":{
+                        "id":"3110000",
+                        "description":"description of locality",
+                        "scheme":"CUATM",
+                        "uri":"locality.uri in EI"
+                    }
+                }
+              },
+              "quantity": 100,
+              "unit": {
+                  "id": "166"
+               }
+
+      }
+    ]
+	},
+  	"planning":
+	{
+    	   "budget":
+		{
+			"period":
+			{
+        		  "startDate": "2022-05-01T00:00:00Z",
+        		  "endDate": "2022-09-30T00:00:00Z"
+      		}
+    	},
+    	 "rationale": "Rationale for EI"
+	},
+        "buyer":
+	{
+      	  "name": "BUYER",
+      	  "identifier":
+		{
+        	"id": "BYR",
+        	"scheme": "MD-IDNO",
+        	"legalName": "Bu Yer",
+        	"uri": "buyer.uri in EI"
+      	},
+          "address": {
+            "streetAddress": "Buyer street",
+            "postalCode": "Buyer postal code",
             "addressDetails": {
                 "country": {
-                    "id": "MD"
+                  "id": "MD"
                 },
                 "region": {
-                    "id": "7100000"
+                  "id": "5700000"
                 },
                 "locality": {
-                    "scheme": "CUATM",
-                    "id": "7101000",
-                    "description": "or.Rîşcani"
+                  "scheme": "other",
+                  "id": "DNPRPTRVSK",
+                  "description": "description of buyer locality"
                 }
             }
         },
-        "additionalIdentifiers": [
-            {
-                "id": "98678687878",
-                "scheme": "MD-IDNO",
-                "legalName": "uStudio",
-                "uri": "ustudio.com"
-            }
-        ],
-        "contactPoint": {
-            "name": "Serghei Tulicii",
-            "email": "t_serrggo77@mail.ru",
-            "telephone": "025694441",
-            "faxNumber": "213123123",
-            "url": "qwdqwd.com"
-        },
-        "details": {
-            "typeOfBuyer": "BODY_PUBLIC",
-            "mainGeneralActivity": "DEFENCE",
-            "mainSectoralActivity": "AIRPORT_RELATED_ACTIVITIES"
-        }
-    }
+      	"additionalIdentifiers":
+		[{
+          	"id": "BY",
+          	"scheme": "MD",
+          	"legalName": "Yer Bu",
+          	"uri": "yerbu.uri"
+        }],
+      	    "contactPoint":
+		{
+        	"name": "Anastasiia",
+        	"email": "anastasiia@gmail.com",
+        	"telephone": "0675667890",
+        	"faxNumber": "0987667899",
+        	"url": "contactPoint.buyer.url"
+      	},
+      	  "details":
+		{
+        	"typeOfBuyer": "BODY_PUBLIC",
+        	"mainGeneralActivity": "PUBLIC_ORDER_AND_SAFETY",
+        	"mainSectoralActivity": "WATER"
+      	}
+	}
 }
--- payloads/create_fs.py
+++ payloads/create_fs.py
@@ -1,108 +1,119 @@
 fs = {
-    "planning": {
-        "budget": {
-            "id": "MD26TRPDAB311120A13489AC",
-            "description": "planning.budget.description",
-            "period": {
-                "startDate": "2020-06-25T16:12:45Z",
-                "endDate": "2022-12-26T16:12:45Z"
-            },
-            "amount": {
-                "amount": 315621.00,
-                "currency": "MDL"
-            },
-            "isEuropeanUnionFunded": True,
-            "europeanUnionFunding": {
-                "projectName": "Name of this project",
-                "projectIdentifier": "projectIdentifier",
-                "uri": "http://uriuri.th"
-            },
-            "project": "project",
-            "projectID": "projectID",
-            "uri": "http://uri.ur"
-        },
-        "rationale": "planning.rationale"
-    },
-    "tender": {
-        "procuringEntity": {
-            "name": "Школа №1 города Кишинёв",
-            "identifier": {
-                "id": "001",
-                "scheme": "MD-IDNO",
-                "legalName": "Direcția Agricultura, Relații Funciare și Cadastru a Consiliului Raional Edineț",
-                "uri": "http://reference.iatistandard.org"
-            },
-            "address": {
-                "streetAddress": "str. Șoseaua Bucovinei 37",
-                "postalCode": "MD4601",
-                "addressDetails": {
-                    "country": {
-                        "id": "MD"
-                    },
-                    "region": {
-                        "id": "4100000"
-                    },
-                    "locality": {
-                        "scheme": "CUATM",
-                        "id": "4101000",
-                        "description": "mun.Chişinău"
-                    }
-                }
-
-            },
-            "additionalIdentifiers": [{
-                "id": "1003600152606",
-                "scheme": "MD-IDNO",
-                "legalName": "IMSP Institutul de Medicina Urgenta",
-                "uri": "http://hrystynivka.miskrada.org.ua/"
-            }],
-            "contactPoint": {
-                "name": "Serviciul achizitii publice",
-                "email": "achizitii@urgenta.md",
-                "telephone": "022250809, 022250707",
-                "faxNumber": "022250809, 022250707",
-                "url": "http://hrystynivka.miskrada.org.ua/"
-            }
-        }
-    },
-    "buyer": {
-        "name": "Школа №1 города Кишинёв",
-        "identifier": {
-            "id": "001",
+  	"tender": {
+      "procuringEntity":
+      {
+        "name": "Procuring Entity in FS",
+        "identifier":
+        {
+            "id": "PRCRNG",
             "scheme": "MD-IDNO",
-            "legalName": "Direcția Agricultura, Relații Funciare și Cadastru a Consiliului Raional Edineț",
-            "uri": "http://reference.iatistandard.org"
+            "legalName": "Legal Procuring Entity in FS",
+            "uri": "PRCRNG.uri"
         },
         "address": {
-            "streetAddress": "str. Șoseaua Bucovinei 37",
-            "postalCode": "MD4601",
+            "streetAddress": "Street of Procuring Entity in FS",
+            "postalCode": "Procuring Entity in FS postal code",
             "addressDetails": {
                 "country": {
-                    "id": "MD"
+                  "id": "MD"
                 },
                 "region": {
-                    "id": "4100000"
+                  "id": "6400000"
                 },
                 "locality": {
-                    "scheme": "CUATM",
-                    "id": "4101000",
-                    "description": "mun.Chişinău"
+                  "scheme": "CUATM",
+                  "id": "6413000",
+                  "description": "description of 6413000"
                 }
             }
-
         },
-        "additionalIdentifiers": [{
-            "id": "1003600152606",
-            "scheme": "MD-IDNO",
-            "legalName": "IMSP Institutul de Medicina Urgenta",
-            "uri": "http://hrystynivka.miskrada.org.ua/"
+        "additionalIdentifiers":
+        [{
+            "id": "PE",
+            "scheme": "MD",
+            "legalName": "PE legalName",
+            "uri": "PE.uri"
         }],
-        "contactPoint": {
-            "name": "Serviciul achizitii publice",
-            "email": "achizitii@urgenta.md",
-            "telephone": "022250809, 022250707",
-            "faxNumber": "022250809, 022250707",
-            "url": "http://hrystynivka.miskrada.org.ua/"
+        "contactPoint":
+        {
+            "name": "PEntity",
+            "email": "pentity@gmail.com",
+            "telephone": "0989807866",
+            "faxNumber": "0675675454",
+            "url": "pentity.uri"
+        }
+      }
+    },
+    "planning":
+	{
+    	"budget":
+		{
+      		"id": "budget in FS",
+      		"description": "description of budget in FS",
+      		"period":
+			{
+        		"startDate": "2022-05-02T00:00:00Z",
+        		"endDate": "2022-09-29T00:00:00Z"
+      		},
+      		"amount":
+			{
+        		"amount": 100000,
+        		"currency": "MDL"
+      		},
+      		"isEuropeanUnionFunded": true,
+			"europeanUnionFunding":
+			{
+      			"projectName": "name of project",
+      			"projectIdentifier": "PRJCT-1",
+      			"uri": "project.uri"
+    		},
+            "project": "PROJECT 1",
+            "projectID": "PRJCT-1",
+            "uri": "PRJCT-1.uri"
+		},
+		"rationale": "rationale for budget in FS"
+	},
+    "buyer":
+    {
+        "name": "buyer name in FS",
+        "identifier":
+        {
+            "id": "FSBYR",
+            "scheme": "MD-IDNO",
+            "legalName": "legalName of FSBYR",
+            "uri": "FSBYR.uri"
+        },
+        "address": {
+            "streetAddress": "Street of FSBYR",
+            "postalCode": "FSBYR postal code",
+            "addressDetails": {
+                "country": {
+                  "id": "MD"
+                },
+                "region": {
+                  "id": "0101000"
+                },
+                "locality": {
+                  "scheme": "CUATM",
+                  "id": "0121000",
+                  "description": "description of 0121000"
+                }
+            }
+        },
+        "additionalIdentifiers":
+        [{
+            "id": "FSBYR",
+            "scheme": "MD",
+            "legalName": "additional legal Name",
+            "uri": "fsbyr.uri"
+        }],
+        "contactPoint":
+        {
+            "name": "FS buyer",
+            "email": "fsbuyer@gmail.com",
+            "telephone": "0986786766",
+            "faxNumber": "0673334323",
+            "url": "fsbuyer.url"
         }
     }
 }
