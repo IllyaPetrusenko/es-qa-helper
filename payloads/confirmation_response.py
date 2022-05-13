da4e80816db991589d917b3779b27006541c18ab
anastasiia <nanamarkova@gmail.com>
2022-05-13 13:39:57 +0200

    nastya5

--- payloads/confirmation_response.py
+++ payloads/confirmation_response.py
@@ -12,12 +12,12 @@ confirmation_response = {
       },
       "businessFunctions": [
         {
-          "type": "authority",
+          "type": "chairman",
           "jobTitle": "jobTitle of RP1",
           "period": {
             "startDate": "2022-01-25T11:16:50Z"
           },
-          "documents": [
+          "!documents": [
             {
               "id": "2e7247f2-66d8-4ebf-96a9-1c5ae5b40216-1652163854914",
               "documentType": "regulatoryDocument",
