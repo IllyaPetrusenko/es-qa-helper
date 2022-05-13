fe1a68bbb9822bdc33a2f72d70fccc650a958956
kate <katyakhaletska@gmail.com>
2022-05-13 11:43:00 +0300

    change

--- payloads/create_bid.py
+++ payloads/create_bid.py
@@ -234,7 +234,7 @@ bid_2 = {
                         "businessFunctions": [
                             {
                                 "id": "",
-                                "type": "authority",
+                                "type": "contactPoint",
                                 "jobTitle": "Owner of the company tyjtytyjtyjtyjtjtyj",
                                 "period": {
                                     "startDate": "2019-10-28T15:20:10Z"
