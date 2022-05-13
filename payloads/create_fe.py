4738ed16f80706a08c74439e9d41432d4826364e
anastasiia <nanamarkova@gmail.com>
2022-05-13 08:56:19 +0200

    nastya5

--- payloads/create_fe.py
+++ payloads/create_fe.py
@@ -8,10 +8,14 @@ fe_auction = {
     "procurementMethodModalities": "PMR in FE",
     "title": "FE tender title",
     "description": "FE tender description",
+    "electronicAuctions": [],
     "secondStage": {
       "minimumCandidates": 1,
       "maximumCandidates": 10
     },
+    "!procurementMethodModalities": [
+      "electronicAuction"
+    ],
     "procurementMethodRationale": "PMR in FE",
     "procuringEntity": {
       "id": "MD-IDNO-00000-001",
@@ -32,7 +36,7 @@ fe_auction = {
               "jobTitle": "boss",
               "period": {
                 "startDate": "2021-01-29T10:58:32Z"
-              }
+              },
             }
           ]
         }
@@ -49,7 +53,7 @@ fe_auction = {
         "documentType": "evaluationCriteria",
         "id": "16009246-c02d-42a1-bfe2-cc4ad6752cac-1642510769694",
         "title": "FE document",
-        "description": "description of FE document",
+        "description": "description of FE document"
       }
     ]
   }
