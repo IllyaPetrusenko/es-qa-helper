5df247912da1ab3b28b253c167f66bb236ea6c71
anastasiia <nanamarkova@gmail.com>
2022-05-16 09:44:08 +0200

    nastya5

--- payloads/create_pcr.py
+++ payloads/create_pcr.py
@@ -1,58 +1,54 @@
 pcr_full_no_catalogue_items_no_auction_no_criteria = {
-  "tender": {
-    "title": "PCR tender title",
-    "description": "PCR tender description",
-    "classification": {
-      "id": "37410000-5",
-      "scheme": "CPV"
-    },
-    "lots": [
-     {
-        "id": "1",
-        "title": "personal computer",
-        "description": "description of personal computer",
-        "internalId": "lot internal id1",
-        "classification": {
-          "id": "37410000-5",
-          "scheme": "CPV"
+        "tender": {
+            "title": "PCR Title",
+            "description": "PCR Description",
+            "classification": {
+                "id": "45200000-9",
+                "scheme": "CPV"
+            },
+            "lots": [
+                {
+                    "id": "1",
+                    "title": "lot one title",
+                    "description": "lot one description",
+                    "internalId": "lot one internalID",
+                    "classification": {
+                        "id": "45200000-9",
+                        "scheme": "CPV"
+                    }
+                }
+            ],
+            "items": [
+                {
+                    "id": "1",
+                    "classification": {
+                        "id": "45200000-9",
+                        "scheme": "CPV"
+                    },
+                    "description": "Item one description",
+                    "quantity": 1000,
+                    "unit": {
+                        "id": "10"
+                    },
+                    "relatedLot": "1",
+                    "internalId": "item one internalID"
+                }
+            ],
+            "tenderPeriod": {
+                "endDate": "2021-09-10T12:48:00Z"
+            },
+            "documents": [
+                {
+                    "id": "00bd3a80-40b9-45ab-817b-8d4fab887ea8-1631102709898",
+                    "documentType": "tenderNotice",
+                    "title": "document title",
+                    "description": "document description",
+                    "relatedLots": [
+                        "1"
+                    ]
+                }
+            ],
+            "awardCriteria": "priceOnly",
+            "awardCriteriaDetails": "automated"
         }
-      }
-    ],
-    "items": [
-      {
-        "id": "1",
-        "classification": {
-          "id": "37410000-5",
-          "scheme": "CPV"
-        },
-         "additionalClassification": {
-          "id": "AA10-8",
-          "scheme": "CPVs"
-        },
-        "description": "description of PCR item1",
-        "quantity": 200,
-        "unit": {
-          "id": "794"
-        },
-        "relatedLot": "1",
-        "internalId": "item internalId1"
-      }
-    ],
-    "tenderPeriod": {
-      "endDate": "2021-10-04T14:05:00Z"
-    },
-    "documents": [
-      {
-        "id": "5c78f85e-c4e7-464a-a4a9-3951520922a8-1652163224146",
-        "documentType": "tenderNotice",
-        "title": "title of PCR document",
-        "description": "description of PCR document",
-        "relatedLots": [
-          "1"
-        ]
-      }
-    ],
-    "awardCriteria": "priceOnly",
-    "awardCriteriaDetails": "automated"
-  }
-}
+    }
