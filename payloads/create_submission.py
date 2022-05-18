045cfbdbc8c92eea35fc8c021c0f03eb4ec8ce17
kate <katyakhaletska@gmail.com>
2022-05-18 11:56:39 +0300

    change

--- payloads/create_submission.py
+++ payloads/create_submission.py
@@ -96,7 +96,7 @@ sub_1 = {
                                 "postalCode": "10056",
                                 "addressDetails": {
                                     "country": {
-                                        "id": "ее",
+                                        "id": "MD",
                                         "description": "country MD",
                                         "scheme": "iso-alpha2"
                                     },
