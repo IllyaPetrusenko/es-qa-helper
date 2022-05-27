10e4aa7dfce209a545fea6a0befd27202f195295
illya.petrusenko@gmail.com <illya.petrusenko@gmail.com>
2022-05-27 12:55:27 +0300

    FR.COM-6.27.5, FR.COM-6.27.4, FR.COM-6.27.3, FR.COM-6.27.2, FR.COM-6.27.1' Test4 covered

--- tests/contracting/tests_getBuyerOwner.py
+++ tests/contracting/tests_getBuyerOwner.py
@@ -372,3 +372,79 @@ class Test3:
                                 'description': 'There must be a record of the organization with the buyer role'
                                             ' for the contract record in the service by cpid and ocid from'
                                                                             ' the request.'}]}
+
+
+@allure.parent_suite('Contracting Integration Tests')
+@allure.suite('Contracting : getBuyerOwner')
+@allure.sub_suite('FR.COM-6.27.5, FR.COM-6.27.4, FR.COM-6.27.3, FR.COM-6.27.2, FR.COM-6.27.1'
+                  ' Сервис должен вернуть объект buyer c атрибутами id, name, owner')
+@allure.severity('Critical')
+@allure.testcase(url='https://ustudio.atlassian.net/wiki/spaces/ES/pages/2658893825/R10.6.27+eContracting+'
+                     'Get+Buyer+Owner',
+                 name='FR.COM-6.27.5 Сервис должен вернуть объект buyer c атрибутами id, name, owner')
+class Test4:
+    @allure.title('FR.COM-6.27.5, FR.COM-6.27.4, FR.COM-6.27.3, FR.COM-6.27.2, FR.COM-6.27.1'
+                  ' Проверить ответ от сервиса для AC')
+    def test_1_positive(self):
+        with allure.step(f'Insert data into DB'):
+            insert = CassandraSession(
+                cassandra_username='caclient',
+                cassandra_password='6AH7vbrkMWnfK',
+                cassandra_cluster='10.0.20.106',
+                json_data=issued_contract
+            )
+            insert.insert_contract_ac_status_issued(cpid='ocds-t1s2t3-MD-1652879244220',
+                                                    ocid='ocds-t1s2t3-MD-1652879244220-AC-1652879488470'
+                                                    )
+
+        with allure.step(f'Prepare request for service'):
+            data['params']['cpid'] = 'ocds-t1s2t3-MD-1652879244220'
+            data['params']['ocid'] = 'ocds-t1s2t3-MD-1652879244220-AC-1652879488470'
+            data['id'] = f'{uuid.uuid4()}'
+            payload = json.dumps(data)
+
+        with allure.step(f'Send request'):
+            r = requests.post(
+                url='http://10.0.20.127:9151/command2',
+                data=payload
+            )
+
+        with allure.step(f'See result'):
+            assert r.status_code == 200
+            assert r.json()['result']['buyer']
+            assert r.json()['result']['buyer']['id']
+            assert r.json()['result']['buyer']['name']
+            assert r.json()['result']['buyer']['owner']
+
+    @allure.title('FR.COM-6.27.5, FR.COM-6.27.4, FR.COM-6.27.3, FR.COM-6.27.2, FR.COM-6.27.1'
+                  ' Проверить ответ от сервиса для PO')
+    def test_2_positive(self):
+        with allure.step(f'Insert data into DB'):
+            insert = CassandraSession(
+                cassandra_username='caclient',
+                cassandra_password='6AH7vbrkMWnfK',
+                cassandra_cluster='10.0.20.106',
+                json_data=issued_contract
+            )
+            insert.insert_contract_po_status_issued(cpid='ocds-t1s2t3-MD-1652879244221',
+                                                    ocid='ocds-t1s2t3-MD-1652879244221-PO-1652879488470'
+                                                    )
+
+        with allure.step(f'Prepare request for service'):
+            data['params']['cpid'] = 'ocds-t1s2t3-MD-1652879244221'
+            data['params']['ocid'] = 'ocds-t1s2t3-MD-1652879244221-PO-1652879488470'
+            data['id'] = f'{uuid.uuid4()}'
+            payload = json.dumps(data)
+
+        with allure.step(f'Send request'):
+            r = requests.post(
+                url='http://10.0.20.127:9151/command2',
+                data=payload
+            )
+
+        with allure.step(f'See result'):
+            assert r.status_code == 200
+            assert r.json()['result']['buyer']
+            assert r.json()['result']['buyer']['id']
+            assert r.json()['result']['buyer']['name']
+            assert r.json()['result']['buyer']['owner']
